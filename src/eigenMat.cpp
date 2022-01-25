4402f8de1b94cd130fa44695233011b6463297b0
siminpan <evolution1163@gmail.com>
2022-01-25 16:24:15 -0600

    add eigenMat.cpp

---
+++ src/eigenMat.cpp
@@ -0,0 +1,49 @@
+// [[Rcpp::depends(RcppArmadillo, RcppEigen)]]
+
+#include <RcppEigen.h>
+
+// [[Rcpp::depends(RcppEigen)]]
+
+using Eigen::Map;                       // 'maps' rather than copies
+using Eigen::MatrixXd;                  // variable size matrix, double precision
+using Eigen::VectorXd;                  // variable size vector, double precision
+using Eigen::SelfAdjointEigenSolver;    // one of the eigenvalue solvers
+
+// not really working
+// https://github.com/ChingChuan-Chen/RcppBlaze/issues/3
+// static_assert(__AVX2__, "AVX 2 is required! Try --march=native or -mavx2");
+
+// [[Rcpp::export]]
+SEXP eigenMatMult(Eigen::MatrixXd A, Eigen::MatrixXd B){
+    Eigen::MatrixXd C = A * B;
+
+    return Rcpp::wrap(C);
+}
+
+// [[Rcpp::export]]
+SEXP eigenMatMulttrans(Eigen::MatrixXd A, Eigen::MatrixXd B){
+    Eigen::MatrixXd C = A * B.transpose();
+
+    return Rcpp::wrap(C);
+}
+
+// [[Rcpp::export]]
+SEXP eigenMapMatMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
+    Eigen::MatrixXd C = A * B;
+
+    return Rcpp::wrap(C);
+}
+
+// [[Rcpp::export]]
+SEXP eigenMapMatMulttrans(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
+    Eigen::MatrixXd C = A * B.transpose();
+
+    return Rcpp::wrap(C);
+}
+
+// [[Rcpp::export]]
+SEXP eigentransMapMatMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
+    Eigen::MatrixXd C = A.transpose() * B;
+
+    return Rcpp::wrap(C);
+}
