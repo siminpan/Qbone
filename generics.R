67bde9ef19a0f4241f67b4ccd5273d04ead2c5d7
siminpan <evolution1163@gmail.com>
2022-02-06 15:31:29 -0600

    add generics2.R add assay class

---
+++ generics.R
@@ -0,0 +1,35 @@
+Samples <- function(x) {
+  UseMethod(generic = 'Samples', object = x)
+}
+
+CreateQboneObject <- function(
+  counts,
+  project = 'CreateQboneObject',
+  assay = 'Bone',
+  names.field = 1,
+  names.delim = '_',
+  meta.data = NULL,
+  ...
+) {
+  UseMethod(generic = 'CreateQboneObject', object = counts)
+}
+
+DefaultAssay <- function(object, ...) {
+  UseMethod(generic = 'DefaultAssay', object = object)
+}
+
+"DefaultAssay<-" <- function(object, ..., value) {
+  UseMethod(generic = 'DefaultAssay<-', object = object)
+}
+
+AddMetaData <- function(object, metadata, col.name = NULL) {
+  UseMethod(generic = 'AddMetaData', object = object)
+}
+
+Idents <- function(object, ... ) {
+  UseMethod(generic = 'Idents', object = object)
+}
+
+"Idents<-" <- function(object, ..., value) {
+  UseMethod(generic = 'Idents<-', object = object)
+}
