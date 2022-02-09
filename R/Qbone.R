093a1248ed31cf7c386105edd10aa7624aed9022
siminpan <evolution1163@gmail.com>
2022-02-09 14:52:00 -0600

    updata Qbone

--- R/Qbone.R
+++ R/Qbone.R
@@ -1,5 +1,8 @@
 #' @include generics.R
 #' @include assay.R
+#' @importFrom methods setClass
+#' @importFrom data.table fread
+#' @useDynLib Qbone
 #'
 NULL
 
@@ -9,20 +12,18 @@ NULL
 
 #' The Qbone Class
 #'
-#' The Qbone object is a representation of data analysis using Quantile Functional Regression using Quantlets (doi: 10.1080/01621459.2019.1609969) for R.
+#' The Qbone object is a representation of data analysis using Quantile
+#' Functional Regression using Quantlets (doi: 10.1080/01621459.2019.1609969)
+#' for R.
 #'
 
 Qbone <- setClass(
   Class = 'Qbone',
   slots = c(
     assays = 'list',
-    # raw.data = 'list',
-    # thin.data = 'list',
     meta.data = 'data.frame',
-    # thin.meta = 'list',
     active.assay = 'character',
     active.ident = 'factor',
-    # lasso.list = 'list',
     graphs = 'list',
     images = 'list',
     project.name = 'character',
@@ -33,13 +34,8 @@ Qbone <- setClass(
   ),
   prototype = c(
     assays = list(),
-    # raw.data = list(),
-    # thin.data = list(),
-    # meta.data = data.frame(id = NULL),
-    # thin.meta = list(), # list(T, "0.1")
     active.assay = NA_character_,
     active.ident = factor(),
-    # lasso.list = list(),
     graphs = list(),
     images = list(),
     project.name = "Qbone",
